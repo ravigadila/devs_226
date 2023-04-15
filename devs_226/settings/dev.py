74e1d0a166bfd2a6b09a140c94b5e41d26412ae3
ravigadila <ravikumar.gadila@gmail.com>
2023-04-15 16:45:51 +0530

    initial commit

---
+++ devs_226/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-md7&qlcbg3^!uk9$^k1#_6=qphd!n)^%jlixb7v6#k+i@mak6$"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
