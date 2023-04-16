732eb8e07c9d2e13cc03a7fe378ea8c21b20dd80
ravigadila <ravikumar.gadila@gmail.com>
2023-04-16 15:06:26 +0530

    blog

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,55 @@
-from wagtail.models import Page
-from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel
+from django.db import models
 
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
+from modelcluster.fields import ParentalKey
+
+from wagtail.models import Page, Orderable
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel, InlinePanel, MultiFieldPanel
+from wagtail.search import index
 
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
 
+    # content_panels = Page.content_panels + [
+    #     FieldPanel('intro')
+    # ]
+    def get_context(self, request):
+
+        # Filter by tag
+        tag = request.GET.get('tag')
+        blogpages = BlogPage.objects.filter(tags__name=tag)
+
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        # blogpages = self.get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
+
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'BlogPage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE
+    )
+
+class BlogPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
     content_panels = Page.content_panels + [
-        FieldPanel('intro')
+        MultiFieldPanel([
+            FieldPanel('date'),
+            FieldPanel('tags'),
+        ], heading="Blog information"),
+        FieldPanel('intro'),
+        FieldPanel('body'),
     ]
